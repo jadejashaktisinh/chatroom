c6ec87bf10c77509fa46d48f3a0f9cebb993bc3c
jadejashaktisinh <bca2023shaktisinh1892@tnraocollege.org>
2024-04-16 00:25:32 +0530

    added

--- script.js
+++ script.js
@@ -66,7 +66,7 @@
                             
                            }
                            
-
-                           
+                            
+                           inputid.value = "";
               }
 
